53e9ae46dddef6b21c5a451f50f23c2c46bebce3
Marie Urmano <marieeurmano@gmail.com>
2017-03-15 02:11:05 -0400

    Study Space Shizz added in

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,19 @@
 const { render } = ReactDOM
 
+const style = {backgroundColor: 'navy', color: 'white', fontFamily: 'century gothic'}
+
 render(
-		<h1 id = 'title'
-			className = 'header'
-			style = {{backgroundColor: 'navy', color: 'white', fontFamily: 'verdana'}}>
-		Hello World
-		</h1>,
+	<div>
+	<h1 id='title'
+		className='header'
+		style={style}>
+	StudySpace	
+	</h1>
+	<h2 id='subtitle'
+		className = 'description'
+		style = {style}>
+	A better way to get your shit done.
+	</h2>
+	</div>,
 	document.getElementById('react-container')
-)
+) //render
